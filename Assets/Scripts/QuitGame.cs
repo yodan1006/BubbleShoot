79e45d9bfbe79984152a77047677a9bab9fada99
yodan <yodanbrandon@gmail.com>
2025-01-23 15:50:48 +0100

    decor scene et script arrangement

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -4,6 +4,9 @@ public class QuitGame : MonoBehaviour
 {
     public void Quit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
         Application.Quit();
     }
 }
